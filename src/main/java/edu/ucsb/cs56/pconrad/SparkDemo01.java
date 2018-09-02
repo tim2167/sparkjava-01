e0a1a6d029e133bcdd1fbb41a7d9b3aadd1078a2
tinghaur@umail.ucsb.edu <tinghaur@umail.ucsb.edu>
2018-09-02 14:11:23 -0700

    Final Update

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -17,8 +17,12 @@ public class SparkDemo01 {
 		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
 		System.out.println("");						  
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
+		String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/tim2167/sparkjava-01'>this github repo</a></p>\n";
 		System.out.println("");
-		spark.Spark.get("/", (req, res) -> "<b>Hello World!</b>\n");
+		spark.Spark.get("/", (req, res) -> html);
+	 spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
 
 	}
 	
